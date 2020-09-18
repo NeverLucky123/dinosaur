63966e29d0b824c0a8be0a5efa24cb53368cb55d
Henry Zhou <43188301+NeverLucky123@users.noreply.github.com>
2020-09-18 12:48:27 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,29 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.x += -1
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.x += 1
 })
-let mySprite : Sprite = null
+let mySprite: Sprite = null
 mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . 3 3 3 . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . 3 3 3 3 3 3 3 3 3 . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . 3 . . . . . . . .
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 3 3 3 . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . 3 3 3 3 3 3 3 3 3 . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
     `, SpriteKind.Player)
 mySprite.setPosition(10, 100)
-forever(function on_forever() {
-    
+forever(function () {
+	
 })
