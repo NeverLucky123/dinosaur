2a9f7feeee16ebfb56fadf64e10ad7e2438418bf
Henry Zhou <43188301+NeverLucky123@users.noreply.github.com>
2020-09-18 12:38:40 -0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+def on_b_pressed():
+    mySprite.x += -1
+controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
+
+def on_a_pressed():
+    mySprite.x += 1
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+mySprite: Sprite = None
 mySprite = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . .
+            . . . . . . 3 3 3 . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . 3 3 3 3 3 3 3 3 3 . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . .
     """),
     SpriteKind.player)
+mySprite.set_position(10, 100)
+
+def on_forever():
+    pass
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,22 +1,29 @@
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 3 3 3 . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . 3 3 3 3 3 3 3 3 3 . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
+controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
+    mySprite.x += -1
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    mySprite.x += 1
+})
+let mySprite : Sprite = null
+mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 3 3 3 . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . 3 3 3 3 3 3 3 3 3 . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . . 
+            . . . . . . . 3 . . . . . . . .
     `, SpriteKind.Player)
 mySprite.setPosition(10, 100)
-forever(function () {
-	
+forever(function on_forever() {
+    
 })
